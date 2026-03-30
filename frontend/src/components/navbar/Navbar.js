5c50162a2481023aa6b728da28d21a3a7c5b8e43
Akash M V <mvakash333@gmail.com>
2026-03-30 10:32:32 +0530

    Remove Login button from Navbar
    Removed the Login button from the navbar component.

--- frontend/src/components/navbar/Navbar.js
+++ frontend/src/components/navbar/Navbar.js
@@ -7,9 +7,6 @@ const Navbar = () => {
        <div className="container m-0 p-0 pt-1 pb-1 d-flex justify-content-between w-100">
        <h3 className='text-white fs-2'>NexusChat</h3>
        <div className='d-flex gap-4'>
-         <button className='btn btn-primary'>
-          <Link to='/login' style={{textDecoration: "none"}} className='text-white'>Login</Link>
-         </button>
        </div>
        </div>
     </div>
